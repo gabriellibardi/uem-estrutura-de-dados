4176e5bc5066e3dc3c8a36a4e7259e035198655e
Gabriel Libardi <gabriellibardilulu@gmail.com>
2024-03-12 21:59:46 -0300

    Finalizando a implementação do trabalho de Arvores e busca

--- Trabalhos/Busca e árvores/trabalho.py
+++ Trabalhos/Busca e árvores/trabalho.py
@@ -96,6 +96,53 @@ def mesmos_elementos(t: Arvore, r: Arvore) -> bool:
         return estao_na_arvore(t, r)
 
 
+def caminhos_maximos(t: Arvore) -> list[list]:
+    r'''
+    Encontra todos os caminhos de tamanho máximo na Árvore Binária *t*.
+
+    Exemplos:
+
+                2
+              /   \
+             8     3  
+            /     / \
+           3     7   5
+            \       /
+             4     2
+
+    >>> esq = No(No(None, 3, No(None, 4, None)), 8, None)
+    >>> dir = No(No(None, 7, None), 3, No(No(None, 2, None), 5, None))
+    >>> t = No(esq, 2, dir)
+    >>> r = cria_avl([])
+    >>> caminhos_maximos(r)
+    []
+    >>> caminhos_maximos(t)
+    [[2, 8, 3, 4], [2, 3, 5, 2]]
+    '''
+    caminhos_max = []
+    for caminho in caminhos(t):
+        if len(caminho) == altura(t) + 1:
+            caminhos_max.append(caminho)
+    return caminhos_max
+
+def qnt_elementos(t: Arvore) -> int:
+    '''
+    Retorna a quantidade de elementos não nulos de uma árvore.
+
+    Exemplos:
+    >>> r = cria_avl([])
+    >>> qnt_elementos(r)
+    0
+    >>> r = cria_avl([3, 5, 6, 10, 43, 54, 100, 432])
+    >>> qnt_elementos(r)
+    8
+    '''
+    if t is None:
+        return 0
+    else:
+        return 1 + qnt_elementos(t.esquerda) + qnt_elementos(t.direita)
+
+
 def estao_na_arvore(t: Arvore, r: Arvore) -> bool:
     '''
     Retorna True caso todos os elementos da AAB *t* estão
@@ -122,24 +169,6 @@ def estao_na_arvore(t: Arvore, r: Arvore) -> bool:
         return estao_na_arvore(t.esquerda, r) \
                and busca_binaria(r, t.valor) \
                and estao_na_arvore(t.direita, r)
-    
-
-def qnt_elementos(t: Arvore) -> int:
-    '''
-    Retorna a quantidade de elementos não nulos de uma árvore.
-
-    Exemplos:
-    >>> r = cria_avl([])
-    >>> qnt_elementos(r)
-    0
-    >>> r = cria_avl([3, 5, 6, 10, 43, 54, 100, 432])
-    >>> qnt_elementos(r)
-    8
-    '''
-    if t is None:
-        return 0
-    else:
-        return 1 + qnt_elementos(t.esquerda) + qnt_elementos(t.direita)
 
 
 def busca_binaria(t: Arvore, valor: int) -> bool:
@@ -170,3 +199,75 @@ def busca_binaria(t: Arvore, valor: int) -> bool:
         return busca_binaria(t.esquerda, valor)
     else: # valor > t.valor
         return busca_binaria(t.direita, valor)
+
+
+def altura(t: Arvore) -> int:
+    r'''
+    Retorna a altura da Árvore *t*.
+    A altura de uma Árvore é definida pelo comprimento do
+    caminho mais longo da raiz até uma folha.
+
+    Exemplos:
+
+                4
+              /   \
+            /       \
+           8         6  
+         /   \      /
+        4      7   5
+                \
+                 1
+
+    >>> esq = No(No(None, 4, None), 8, No(None, 7, No(None, 1, None)))             
+    >>> dir = No(No(None, 5, None), 6, None)
+    >>> r = No(esq, 4, dir)
+    >>> altura(r)
+    3
+    >>> altura(esq)
+    2
+    >>> altura(dir)
+    1
+    >>> r = No(None, 2, None)
+    >>> altura(r)
+    0
+    >>> r = cria_avl([])
+    >>> altura(r)
+    -1
+    '''
+    if t is None:
+        return -1
+    else:
+        return 1 + max(altura(t.esquerda), altura(t.direita))
+    
+def caminhos(t: Arvore) -> list[list]:
+    r'''
+    Encontra todos os caminhos da Árvore *t* partindo da raiz.
+
+    Exemplos:
+
+                2
+              /   \
+             8     3  
+            /     / \
+           3     7   5
+            \       /
+             4     2
+
+    >>> esq = No(No(None, 3, No(None, 4, None)), 8, None)
+    >>> dir = No(No(None, 7, None), 3, No(No(None, 2, None), 5, None))
+    >>> t = No(esq, 2, dir)
+    >>> r = cria_avl([])
+    >>> caminhos(r)
+    []
+    >>> caminhos(t)
+    [[2, 8, 3, 4], [2, 3, 7], [2, 3, 5, 2]]
+    '''
+    if t is None:
+        return []
+    elif t.esquerda is None and t.direita is None:
+        return [[t.valor]]
+    else:
+        caminhos_dir_esq = caminhos(t.esquerda) + caminhos(t.direita)
+        for caminho in caminhos_dir_esq:  
+            caminho.insert(0, t.valor)
+        return caminhos_dir_esq
